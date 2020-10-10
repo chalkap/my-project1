07c6de5acdc5910119d647c338e33607dbbef1c0
chalani <chalani.17@business.mrt.ac.lk>
2020-10-10 12:25:13 +0530

    My first python file

---
+++ python01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+
+
+# In[ ]:
+
+
+
+
