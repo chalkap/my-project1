3f730511ac3d340d97b2455ea49ed9c96cc6fbd7
Kalani-cj <72610841+Kalani-cj@users.noreply.github.com>
2020-10-10 00:41:01 -0700

    Updated my project 1
    Checking whether numbers are small or large

--- python01.py
+++ python01.py
@@ -1,17 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+print("Check if Number is odd or even")
 num = int(input("Enter a number: "))
 if (num % 2) == 0:
    print("{0} is Even".format(num))
 else:
    print("{0} is Odd".format(num))
 
+print("Check if number is large or small")
 
-# In[ ]:
+if num>=1 and num<=10:
+    print("too low");
+
+elif num>=11 and num<=20:
+    print("medium");   
+
+elif num>=21 and num<=30:
+    print("large");
+
+else:
+    print("too large")
+
+
 
 
 
